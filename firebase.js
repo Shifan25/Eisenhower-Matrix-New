f68a35ed2a09b9f21601e3fb86a817f3d93bd63c
Shifan25 <shifanishak@gmail.com>
2025-07-25 00:54:19 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,40 +1,17 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getFirestore, collection, addDoc, doc, updateDoc, deleteDoc,
-  onSnapshot, query, where
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "PASTE-HERE",
-  authDomain: "PASTE-HERE",
-  projectId: "PASTE-HERE",
-  storageBucket: "PASTE-HERE",
-  messagingSenderId: "PASTE-HERE",
-  appId: "PASTE-HERE"
+  apiKey: "AIzaSyDIaC6hiNxTL2RlmprzQFzhcfKR7R-_HH0",
+  authDomain: "eisenhower-matrix-f4901.firebaseapp.com",
+  projectId: "eisenhower-matrix-f4901",
+  storageBucket: "eisenhower-matrix-f4901.firebasestorage.app",
+  messagingSenderId: "700179285214",
+  appId: "1:700179285214:web:bcf86cb058a7fbf268ae4f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// *** THIS IS THE “USER ID”. Just choose a string and keep it the same on all devices. ***
-const USER_ID = "shifan@example.com";
-
-// Listen to tasks in Firestore for this user
-export function watchTasks(callback) {
-  const q = query(collection(db, "tasks"), where("user", "==", USER_ID));
-  return onSnapshot(q, snap => {
-    const tasks = snap.docs.map(d => ({ id: d.id, ...d.data() }));
-    callback(tasks);
-  });
-}
-
-export async function addTaskRemote(task) {
-  await addDoc(collection(db, "tasks"), { ...task, user: USER_ID });
-}
-export async function updateTaskRemote(task) {
-  await updateDoc(doc(db, "tasks", task.id), task);
-}
-export async function deleteTaskRemote(id) {
-  await deleteDoc(doc(db, "tasks", id));
-}
